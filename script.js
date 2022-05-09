9893cd8c2ece4544268ad602e27c1450e503502a
VladimirVolvakov <dr.volvakov@gmail.com>
2022-05-09 21:04:22 +0300

    feat: add basic structure of Keyboard module

---
+++ script.js
@@ -0,0 +1,33 @@
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+    properties: {
+        value: "",
+        capsLock: false
+    },
+    init() {
+
+    },
+    _createKeys() {
+
+    },
+    _triggerEvent(handlerName) {
+
+    },
+    _toggleCapsLock() {
+
+    },
+    openKeyboard(initialValue, oninput, onclose) {
+
+    },
+    closeKeyboard() {
+
+    }
+};
